a3f71e29764dfde30ab0d4353dac8b4bc0b80c89
Hans-Jörg Bibiko <hans_joerg_bibiko@eva.mpg.de>
2022-01-19 08:56:35 +0100

    updated and fine-tuned splash; pep8

--- Resources/splash.py
+++ Resources/splash.py
@@ -1,35 +1,34 @@
-import wx, sys, os
+import wx
+import sys
+import os
 import Resources.variables as vars
 
+
 def GetRoundBitmap(w, h, r=10):
-    maskColour = wx.Colour(0,0,0)
-    shownColour = wx.Colour(5,5,5)
-    b = wx.EmptyBitmap(w,h)
+    maskColour = wx.Colour(0, 0, 0)
+    shownColour = wx.Colour(5, 5, 5)
+    b = wx.EmptyBitmap(w, h)
     dc = wx.MemoryDC(b)
     dc.SetBrush(wx.Brush(maskColour))
-    dc.DrawRectangle(0,0,w,h)
+    dc.DrawRectangle(0, 0, w, h)
     dc.SetBrush(wx.Brush(shownColour))
     dc.SetPen(wx.Pen(shownColour))
-    dc.DrawCircle(w/2,h/2,w/2)
+    dc.DrawCircle(w/2, h/2, w/2 - 5)
     dc.SelectObject(wx.NullBitmap)
     b.SetMaskColour(maskColour)
     return b
 
-def GetRoundShape(w, h, r=10):
-    if sys.version_info[0] < 3:
-        return wx.RegionFromBitmap(GetRoundBitmap(w,h,r))
-    else:
-        return wx.Region(GetRoundBitmap(w,h,r))
 
 class ZyneSplashScreen(wx.Frame):
     def __init__(self, parent, img, mainframe):
         display = wx.Display(0)
         size = display.GetGeometry()[2:]
-        wx.Frame.__init__(self, parent, -1, "", pos=(-1, size[1]/6),
-                         style = wx.FRAME_SHAPED | wx.SIMPLE_BORDER | wx.FRAME_NO_TASKBAR | wx.STAY_ON_TOP)
+        wx.Frame.__init__(
+            self, parent, -1, "", pos=(-1, size[1]/6),
+            style=wx.FRAME_SHAPED | wx.SIMPLE_BORDER | wx.FRAME_NO_TASKBAR | wx.STAY_ON_TOP)
 
         self.Bind(wx.EVT_PAINT, self.OnPaint)
-        
+
         self.mainframe = mainframe
         self.bmp = wx.Bitmap(os.path.join(img), wx.BITMAP_TYPE_PNG)
         self.w, self.h = self.bmp.GetWidth(), self.bmp.GetHeight()
@@ -48,20 +47,20 @@ class ZyneSplashScreen(wx.Frame):
         self.Center(wx.HORIZONTAL)
         if sys.platform == 'win32':
             self.Center(wx.VERTICAL)
-            
+
         wx.CallAfter(self.Show)
-        
+
     def SetWindowShape(self, *evt):
-        r = GetRoundShape(self.w - 3, self.h - 3)
+        r = wx.Region(GetRoundBitmap(self.w, self.h))
         self.hasShape = self.SetShape(r)
 
     def OnPaint(self, evt):
-        w,h = self.GetSize()
+        w, h = self.GetSize()
         dc = wx.PaintDC(self)
         dc.SetPen(wx.Pen("#000000"))
         dc.SetBrush(wx.Brush("#000000"))
-        dc.DrawRectangle(0,0,w,h)
-        dc.DrawBitmap(self.bmp, 0,0,True)
+        dc.DrawRectangle(0, 0, w, h)
+        dc.DrawBitmap(self.bmp, 0, 0, True)
         dc.SetTextForeground("#000000")
         font = dc.GetFont()
         ptsize = font.GetPointSize()
@@ -69,19 +68,21 @@ class ZyneSplashScreen(wx.Frame):
             pass
         else:
             font.SetFaceName("Monaco")
-            font.SetPointSize(ptsize+3)
+            font.SetPointSize(ptsize + 3)
         dc.SetFont(font)
-        dc.DrawLabel("Modular Soft Synthesizer", wx.Rect(20, 230, 400, 18), wx.ALIGN_LEFT)
+        dc.DrawLabel("Zyne_B", wx.Rect(50, 230, 400, 18), wx.ALIGN_LEFT)
+        dc.DrawLabel("Modular Soft Synthesizer", wx.Rect(70, 250, 400, 18), wx.ALIGN_LEFT)
         if vars.constants["PLATFORM"] == "win32":
             pass
         else:
             font.SetPointSize(ptsize+1)
         dc.SetFont(font)
-        dc.DrawLabel("Olivier Bélanger", wx.Rect(0, 305, 400, 15), wx.ALIGN_CENTER)
+        dc.DrawLabel("Olivier Bélanger (ajaxsoundstudio)",
+                     wx.Rect(0, 305, 400, 15), wx.ALIGN_CENTER)
         dc.DrawLabel("Hans-Jörg Bibiko", wx.Rect(0, 320, 400, 15), wx.ALIGN_CENTER)
-        dc.DrawLabel(f"ajaxsoundstudio, {vars.constants['YEAR']}", wx.Rect(0, 340, 400, 15), wx.ALIGN_CENTER)
-        dc.DrawLabel(f"v. {vars.constants['VERSION']}", wx.Rect(0, 360, 400, 15), wx.ALIGN_CENTER)
-     
+        dc.DrawLabel(f"Version {vars.constants['VERSION']} - {vars.constants['YEAR']}",
+                     wx.Rect(0, 340, 400, 15), wx.ALIGN_CENTER)
+
     def OnClose(self):
         self.mainframe.Show()
         self.Destroy()
